4cbaaaec0f3d91fd64856da91cbe8016dcad9916
Roma Bulani <romabulani@gmail.com>
2022-03-25 23:01:12 +0530

    feat : added category sorting

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -10,5 +10,12 @@ export const reducer = (state, action) => {
         ...state,
         categories: action.payload.categories,
       };
+    case "CATEGORY":
+      return {
+        ...state,
+        category: action.payload.category,
+      };
+    default:
+      return state;
   }
 };
