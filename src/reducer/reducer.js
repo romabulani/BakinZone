27399d3c98fd9ab1827c006143b650d819192e2a
Roma Bulani <romabulani@gmail.com>
2022-03-25 16:49:16 +0530

    refactor : removed console logs

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,13 +1,11 @@
 export const reducer = (state, action) => {
   switch (action.type) {
     case "SET_VIDEOS":
-      console.log("in set vids", action.payload);
       return {
         ...state,
         videos: action.payload.videos,
       };
     case "SET_CATEGORIES":
-      console.log(action, "in set cat");
       return {
         ...state,
         categories: action.payload.categories,
