af015b0c17ef1fe96a5e244e0206a8a180e0c38d
Roma Bulani <romabulani@gmail.com>
2022-03-30 18:53:41 +0530

    refactor: searchbar to use params

--- src/components/videolisting/Videos.jsx
+++ src/components/videolisting/Videos.jsx
@@ -1,10 +1,30 @@
 import { useData } from "contexts";
 import "./videos.css";
 import { VideoCard } from "./VideoCard";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Videos() {
   const { state, dispatch, setSearchBarText } = useData();
+  const { search } = useLocation();
+  const [searchedVideos, setSearchedVideos] = useState([]);
+
+  const getSearchedVideos = () => {
+    let filteredVideos = [];
+    // To get the string after = in URL
+    const query = decodeURIComponent(search.split("=")[1]);
+    filteredVideos = state.videos.filter(
+      (video) =>
+        video.title.toLowerCase().includes(query.toLowerCase()) ||
+        video.category.toLowerCase().includes(query.toLowerCase())
+    );
+    setSearchedVideos(filteredVideos);
+    setSearchBarText("");
+  };
+
+  useEffect(() => {
+    if (search.length > 0) getSearchedVideos();
+  }, [search]);
 
   const dispatchHandler = (category) => {
     dispatch({ type: "CATEGORY", payload: { category } });
@@ -18,61 +38,69 @@ function Videos() {
         (perVideo) => perVideo.category === state.category
       );
 
-    if (state.searchText !== "")
-      filteredVideos = state.videos.filter(
-        (video) =>
-          video.title.toLowerCase().includes(state.searchText.toLowerCase()) ||
-          video.category.toLowerCase().includes(state.searchText.toLowerCase())
-      );
-
     return filteredVideos;
   }
 
   return (
     <>
-      <div>
-        <div className="categories">
-          {state.categories.map((category) => (
-            <span
-              className={`chip category-chip ${
-                category.categoryName === state.category
-                  ? "active-category"
-                  : null
-              }`}
-              key={category._id}
-              onClick={() => dispatchHandler(category.categoryName)}
-            >
-              {category.categoryName}
-            </span>
-          ))}
-        </div>
-      </div>
-
-      <div className="flex-row-center">
-        {getFilteredVideos().length > 0 ? (
-          <div className="videos-container">
-            {getFilteredVideos().map((video) => (
-              <VideoCard video={video} key={video._id}></VideoCard>
+      {!search && (
+        <div>
+          <div className="categories">
+            {state.categories.map((category) => (
+              <span
+                className={`chip category-chip ${
+                  category.categoryName === state.category
+                    ? "active-category"
+                    : null
+                }`}
+                key={category._id}
+                onClick={() => dispatchHandler(category.categoryName)}
+              >
+                {category.categoryName}
+              </span>
             ))}
           </div>
-        ) : (
+          <div className="flex-row-center">
+            <div className="videos-container">
+              {getFilteredVideos().map((video) => (
+                <VideoCard video={video} key={video._id}></VideoCard>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+
+      {search && searchedVideos.length > 0 && (
+        <div className="flex-column">
+          <div className="flex-column-center search-header">
+            <div>
+              {`Search Results for "${state.searchText}" - ${searchedVideos.length} videos`}
+            </div>
+          </div>
+          <div className="flex-row-center">
+            <div className="videos-container">
+              {searchedVideos.map((video) => (
+                <VideoCard video={video} key={video._id}></VideoCard>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+
+      {search && searchedVideos.length === 0 && (
+        <div className="flex-row-center">
           <div className="margin-container flex-column-center padding-top-8">
             {`No Search Results found for "${state.searchText}"`}
-            <button
+            <Link
               className="btn btn-primary no-link-decoration inline-flex-center"
-              onClick={() => {
-                setSearchBarText("");
-                dispatch({
-                  type: "SET_SEARCH_TEXT",
-                  payload: { searchText: "" },
-                });
-              }}
+              to="/videos"
             >
               Explore
-            </button>
+            </Link>
           </div>
-        )}
-      </div>
+        </div>
+      )}
+
       <Outlet />
     </>
   );
